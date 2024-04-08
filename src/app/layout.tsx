c099882bd013c33c9c4f78d8b8771051f5bfe4d1
nzkks <kkshanthosh@gmail.com>
2024-04-09 00:11:58 +1200

    Layout: Add simple header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <header>Header</header>
         <StoreProvider>{children}</StoreProvider>
+        <footer>Footer</footer>
       </body>
     </html>
   );
